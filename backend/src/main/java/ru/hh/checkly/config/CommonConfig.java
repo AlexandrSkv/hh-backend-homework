f765a84e325281cf7ff2384c1571ccd87e218a81
sasha_rus <sasha_rus@list.ru>
2022-04-18 12:46:05 +0300

    BACK-01 создана заготовка для NaB приложения

---
+++ backend/src/main/java/ru/hh/checkly/config/CommonConfig.java
@@ -0,0 +1,24 @@
+package ru.hh.checkly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import ru.hh.nab.hibernate.MappingConfig;
+import ru.hh.nab.starter.NabCommonConfig;
+import ru.hh.checkly.resource.ExampleResource;
+
+@Configuration
+@Import({
+  // import your beans here
+  ExampleResource.class,
+  NabCommonConfig.class
+})
+public class CommonConfig {
+
+  @Bean
+  public MappingConfig mappingConfig() {
+    MappingConfig mappingConfig = new MappingConfig();
+    mappingConfig.addPackagesToScan("ru.hh.school.entity");
+    return mappingConfig;
+  }
+}
