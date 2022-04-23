6f5ae481cfb0a6fa2101d568e5196ecf96165ae8
strelchm <strelchm@yandex.ru>
2022-04-23 18:37:40 +0700

    BACK-12 создан сервис отправки email-сообщений

--- backend/src/main/java/ru/hh/checkly/config/CommonConfig.java
+++ backend/src/main/java/ru/hh/checkly/config/CommonConfig.java
@@ -3,6 +3,7 @@ package ru.hh.checkly.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import ru.hh.checkly.service.impl.MailSenderServiceImpl;
 import ru.hh.nab.hibernate.MappingConfig;
 import ru.hh.nab.starter.NabCommonConfig;
 import ru.hh.checkly.resource.ExampleResource;
@@ -11,7 +12,9 @@ import ru.hh.checkly.resource.ExampleResource;
 @Import({
   // import your beans here
   ExampleResource.class,
-  NabCommonConfig.class
+  NabCommonConfig.class,
+  MailSenderServiceImpl.class,
+  MailConfig.class,
 })
 public class CommonConfig {
 
---
+++ backend/src/main/java/ru/hh/checkly/service/MailSenderService.java
@@ -0,0 +1,10 @@
+package ru.hh.checkly.service;
+
+import ru.hh.checkly.dto.MailMessage;
+
+import javax.mail.MessagingException;
+
+public interface MailSenderService {
+
+    void sendEmailWithAttachment(MailMessage message) throws MessagingException;
+}
